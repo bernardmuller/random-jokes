bd83e94cbe7e550897b5e1dd1a34794bbd37ba84
Bernard Muller <b.mullerjnr@gmail.com>
2023-09-07 17:36:52 +0200

    feat: read json file

--- main.go
+++ main.go
@@ -1,12 +1,22 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
 
 	"github.com/julienschmidt/httprouter"
 )
+
+const FILE = "./data.json"
+
+func checkNilError(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
 
 func getPort(port string) string {
 	envPort := os.Getenv("PORT")
@@ -17,8 +27,16 @@ func getPort(port string) string {
 	return ":" + port
 }
 
+type StringList []string
+
 func main() {
 	router := httprouter.New()
+	data_file_content, err := os.ReadFile(FILE)
+	checkNilError(err)
+
+	var data_json StringList
+	json.Unmarshal(data_file_content, &data_json)
+	fmt.Printf("%T: %v \n", data_json, data_json)
 
 	port := getPort("8080")
 	log.Fatal(http.ListenAndServe("0.0.0.0"+port, router))
