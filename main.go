332e05802466b4464d2f4af862e20041a83e9ad2
Bernard Muller <b.mullerjnr@gmail.com>
2023-09-07 15:16:46 +0200

    feat: initialise server

--- main.go
+++ main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+func getPort(port string) string {
+	envPort := os.Getenv("PORT")
+	if envPort != "" {
+		return ":" + envPort
+	}
+
+	return ":" + port
+}
 
 func main() {
-	fmt.Println("Hello Jokes!")
+	router := httprouter.New()
+
+	port := getPort("8080")
+	log.Fatal(http.ListenAndServe("0.0.0.0"+port, router))
 }
